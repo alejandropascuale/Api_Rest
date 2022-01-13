e978f7daec643c3ab8c08dc475a7c58afa61337d
alejandropascuale <apascuale@outlook.com>
2022-01-13 16:52:06 -0300

    model of profile user finish

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,4 +5,24 @@ from django.contrib.auth.models import PermissionsMixin
 
 class UserProfile (AbstractBaseUser, PermissionsMixin):
     ''' Modelo Base de datos para Usuarios en el sistema '''
-    
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    object = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        ''' Obtener nombre completo del usuario '''
+        return self.name
+
+    def get_short_name(self):
+        ''' Obtener nombre corto del usuario '''
+        return self.name
+
+    def __str__(self):
+        ''' Retornar cadena representando nuestro usuario '''
+        return self.email
