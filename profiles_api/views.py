b4070e427b307f59189c41c94615456ed1a7fb3f
alejandropascuale <apascuale@outlook.com>
2022-01-14 11:41:19 -0300

    create view for change message

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,15 @@
+from email import message
+import re
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializer
 
 class HelloApiView(APIView):
     # API View de prueba
+
+    serializer_class = serializer.HelloSerializer
+
     def get(self, request, format=None):
         # Retornar lista de caracteristicas del API View
         an_apiview = [
@@ -13,3 +20,17 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        # Crea un mensaje con nuestro nombre
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
